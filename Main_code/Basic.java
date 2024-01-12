075bdb3b0b61d4b326d0baa9253acec66aae3ab9
Ashish Kumar Nishad <118613984+ashishkkunishad@users.noreply.github.com>
2024-01-12 20:10:52 +0530

    First commit

---
+++ Main_code/Basic.java
@@ -0,0 +1,14 @@
+class Hello{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        int num=5;
+        int num2=6;
+        System.out.println(num);
+        System.out.println(num);
+        float num3=10.5f;
+        System.out.println(num3);
+
+
+
+    }
+}
