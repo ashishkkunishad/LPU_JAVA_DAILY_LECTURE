30408da1772a9379a2d7edd25e954404f0a8bb18
Ashish Kumar Nishad <118613984+ashishkkunishad@users.noreply.github.com>
2024-01-18 20:13:54 +0530

    update

---
+++ Main_code/Arithmatic/Arithmatic.java
@@ -0,0 +1,21 @@
+class arth{
+    public static void main(String args[]){
+        int num1=5;
+        int num2=8;
+        // int sum=num2+num1;
+        // int sub=num2-num1;
+        // double div=num2/num2;
+        // int mul=num1*num2;
+        // int mod=num1%num2;
+        // System.out.println(sum);
+        // System.out.println(sub);
+        // System.out.println(div);
+        // System.out.println(mul); 
+        num1++;
+        System.out.println(num1);
+        int output=num1++;
+        System.out.println(num1);
+
+
+    }
+}
--- Main_code/type_casting.java
+++ Main_code/type_casting.java
@@ -1,8 +1,43 @@
-class Hello{
+class Helloo{
     public static void main(String args[]){
         int num1=5;
-        double num2=num1;
-        System.out.println(num2);
+        // double num2=num1;
+        // System.out.println(num2);
+        int num2=8;
+        int sum=num2+num1;
+        int sub=num2-num1;
+        double div=num2/num2;
+        int mul=num1*num2;
+        int mod=num1%num2;
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(div);
+        System.out.println(mul);
+        class Hello{
+            public static void main(String args[]){
+                int num1=5;
+                // double num2=num1;
+                // System.out.println(num2);
+                int num2=8;
+                int sum=num2+num1;
+                int sub=num2-num1;
+                double div=num2/num2;
+                int mul=num1*num2;
+                int mod=num1%num2;
+                System.out.println(sum);
+                System.out.println(sub);
+                System.out.println(div);
+                System.out.println(mul);
+                System.out.println(mod);
+
+                
+        
+        
+        
+            }
+        }
+
+
 
     }
 }
