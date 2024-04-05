82c4a5101a4ec085576135b5c3ec275dd5200bb1
Ashish Kumar Nishad <118613984+ashishkkunishad@users.noreply.github.com>
2024-04-05 20:58:55 +0530

    update

--- Main_code/Collection/Treeset.java
+++ Main_code/Collection/Treeset.java
@@ -7,8 +7,11 @@ class lpu{
         TreeSet<Integer> set1=new TreeSet<>();
 
         set1.add(2);
-        set1.add(3);
+        set1.add(4);
         set1.add(5);
+        set1.add(6);
+
+
         System.out.println(set1);
 
         // Iterator<Integer> itr= set1.iterator();
@@ -16,8 +19,24 @@ class lpu{
         //     System.out.println(itr.next());
         // }
 
-        System.out.println(set1.remove(2));
-        System.out.println(set1);
+        // set1.remove(2);
+        // System.out.println(set1);
+       // System.out.println(set1.first());//first smallest element
+       // System.out.println(set1.last());//lasthighest element
+        //System.out.println(set1.higher(4));//Returns the least element in this set
+                // strictly greater than the given element, or null if there is no such element
+        // System.out.println(set1.lower(4));
+        // System.out.println(set1.ceiling(6));
+        // System.out.println(set1.floor(3));
+        // System.out.println(set1.pollFirst());
+        // System.out.println(set1.pollLast());
+        //System.out.println(set1.headSet(5));//gives smaller no. of given no.
+        //System.out.println(set1.tailSet(4));//give elements greater and equal than no.
+        System.out.println(set1.subSet(2,true, 5, true));
+
+
+
+
 
 
 
